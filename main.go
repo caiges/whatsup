b7707b7e8d529f556ef43bc0c5619a9089ef9064
caiges <caigesn@gmail.com>
2015-05-28 23:10:42 -0700

    Dump name and version

--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
-	"fmt"
-	"os"
 	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
 )
 
 type Config struct {
-	Urls	[]string `json:"urls"` 
+	Urls []map[string]string
+}
+
+type Version struct {
+	Version string `json:"version"`
+}
+
+func ParseVersion(data io.Reader) (Version, error) {
+	decoder := json.NewDecoder(data)
+	v := Version{}
+	err := decoder.Decode(&v)
+
+	return v, err
 }
 
 func main() {
@@ -19,7 +33,17 @@ func main() {
 	err = decoder.Decode(&config)
 	checkErr(err)
 
-	fmt.Println(config.Urls)
+	for _, url := range config.Urls {
+		resp, err := http.Get(url["url"])
+		checkErr(err)
+
+		if resp.StatusCode == 200 {
+			version, err := ParseVersion(resp.Body)
+			checkErr(err)
+
+			fmt.Printf("%s -- %s\n", url["name"], version.Version)
+		}
+	}
 }
 
 func checkErr(err error) {
